679602855d06770276d279de8bfab7c638947d96
xxx <25452644+bartoshevich@users.noreply.github.com>
2022-07-14 23:26:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,53 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let u = [];
+    let s = [];
+    let result = [];
+      if (expr === undefined || expr.length === 0 ) {
+        return [];
+       } else {
+        let symbolNumber = expr.length / 10;
+         for (let i = 0; i < symbolNumber; i++) {       
+            u[i] = expr.slice(10*i,(10*i) + 10);     
+        }
+    }
+  
+    for (let letter of u) {
+      if (letter === '**********') {
+        letter = ' ';
+      } else {
+      
+      for (let i = 0; i < 5; i++) {  
+             s[i] = letter.slice(2*i,(2*i) + 2);      
+        }
+  
+    for (let i = 0; i < 5; i++) {  
+          if (s[i] === '00') {
+          s[i] = '';
+        } else if (s[i] === '10') {
+          s[i] = '.';
+        } else if (s[i] === '11') {
+          s[i] = '-';
+        } 
+        }  
+     letter = s.join('');
+  
+    for (let [key, prop] of Object.entries(MORSE_TABLE)) {
+      if (key === letter) {
+        letter = prop;
+      }
+         
+    }
+    
+    }
+  
+   result += letter;
+     
+  }
+  return result; 
+  
+  }
 
 module.exports = {
     decode
